b125df229c1ee4803b0ba1068bdac0f40e4aa2fe
aniruudhh <93992771+aniruudhh@users.noreply.github.com>
2022-09-11 19:07:59 +0530

    Add files via upload

---
+++ Phase 3 Non Assisted Projects/retrieveusersandupdate/src/main/java/com/simplilearn/dao/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.simplilearn.dao;
+
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.simplilearn.beans.Employee;
+
+public class EmployeeMapper implements RowMapper<Employee>{
+
+	public Employee mapRow(ResultSet rs, int rowNum)throws SQLException{
+		Employee employee =new Employee();
+		employee.setEid(rs.getInt("eid"));
+		employee.setName(rs.getString("name"));
+		employee.setDesignation(rs.getString("designation"));
+		employee.setSalary(rs.getFloat("salary"));
+		return employee;
+	}
+
+	
+	
+
+}
